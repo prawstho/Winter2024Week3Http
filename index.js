24acf89abf7077f930a888eaa3b926f0f12ebca3
Peter <113168584+prawstho@users.noreply.github.com>
2024-01-18 10:08:45 -0330

    working http server with fetchFile function

--- index.js
+++ index.js
@@ -1,30 +1,43 @@
 const http = require('http');
 const fs = require('fs');
-global.DEBUG = true;
+global.DEBUG = false;
 
 const server = http.createServer((request, response) => {
   if(DEBUG) console.log('Request Url:', request.url);
+  let filename = 'index.html';
   switch(request.url) {
     case '/':
       if(DEBUG) console.log('Root Route');
-      response.writeHead(200, { 'Content-Type': 'text/html' });
-      response.end('<h1>Welcome to the Root Route</h1>');
+      filename = 'index.html';
+      fetchFile(filename);
       break;
     case '/home':
       if(DEBUG) console.log('Home Route');
-      response.writeHead(200, { 'Content-Type': 'text/html' });
-      response.end('<h1>Welcome to the Home Page</h1>');
+      filename = 'home.html';
+      fetchFile(filename);
       break;
     case '/about':
       if(DEBUG) console.log('About Route');    
-      response.writeHead(200, { 'Content-Type': 'text/html' });
-      response.end('<h1>Welcome to the About Page</h1>');
+      filename = 'about.html';
+      fetchFile(filename);
       break;
     default:
       if(DEBUG) console.log('404 Not Found');
       response.writeHead(404, { 'Content-Type': 'text/plain' });
       response.end('404 Not Found');
+      break;
   }
+  function fetchFile(fileName) {
+    fs.readFile(fileName, (error, content) => {
+      if(error) {
+        response.writeHead(500, { 'Content-Type': 'text/plain' });
+        response.end('500 Internal Server Error');
+      } else {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.end(content, 'utf-8');
+      }
+    });
+  };
 });
 
 server.listen(3000, () => {
