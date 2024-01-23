7da5216a8a37a746406f877268bffcabe36da27d
Peter <113168584+prawstho@users.noreply.github.com>
2024-01-23 14:35:14 -0330

    created routes

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const EventEmitter = require('events');
 class MyEmitter extends EventEmitter {}
 const myEmitter = new EventEmitter(); 
 
+const routes = require('./routes.js');
+
 global.DEBUG = true;
 
 myEmitter.on('route', (url) => {
@@ -26,9 +28,14 @@ const server = http.createServer((request, response) => {
     case '/':
       path += 'index.html';
       myEmitter.emit('route', path);
-      fetchFile(path);
+      routes.indexPage(path, response);
       break;
     case '/home':
+      path += 'home.html';
+      myEmitter.emit('route', path);
+      routes.homePage(path, response);
+      break;
+    case '/about-me':
       response.statusCode = 301;
       response.setHeader('Location', '/about');
       response.end();
@@ -40,7 +47,7 @@ const server = http.createServer((request, response) => {
     case '/about':
       path += 'about.html';
       myEmitter.emit('route', path);
-      fetchFile(path);
+      routes.aboutPage(path, response);
       break;
     default:
       if(DEBUG) console.log('404 Not Found');
@@ -48,17 +55,6 @@ const server = http.createServer((request, response) => {
       response.end('404 Not Found');
       break;
   }
-  function fetchFile(fileName) {
-    fs.readFile(fileName, (error, content) => {
-      if(error) {
-        response.writeHead(500, { 'Content-Type': 'text/plain' });
-        response.end('500 Internal Server Error');
-      } else {
-        response.writeHead(200, { 'Content-Type': 'text/html' });
-        response.end(content, 'utf-8');
-      }
-    });
-  };
 });
 
 server.listen(3000, () => {
